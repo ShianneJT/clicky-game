acaa690ce44287390a02c35de8130f342a0c2fdf
Shianne <ShianneJT@gmail.com>
2020-07-14 12:33:48 -0600

    Cards are shuffeling

--- src/App.js
+++ src/App.js
@@ -13,17 +13,49 @@ class App extends React.Component {
         topScore: 0,
     };
 
-    handleCardClick = id => {
-        // do stuff
+    handleImgClick = id => {
+        const clickedImg = this.state.characters.filter(character => character.id === id);
+        console.log('click')
+            if (clickedImg[0].clicked) {
+                console.log('Already clicked that one!');
+                this.setState({
+                    correctGuesses: 0,
+                });
+                this.handleCardShuffle();
+            }
+            this.handleCardShuffle();
     }
 
     handleCardShuffle = () => {
+        let cards = this.state.characters;
+        for (let i = 0; i < cards.length - 1; i++) {
+            let j = i + Math.floor(Math.random() * (cards.length - i));
 
-    }
+            const temp = cards[j];
+            cards[j] = cards[i];
+            cards[i] = temp;
+        };
+            this.setState({ characters: cards });
+    };
 
-    resetStates = () => {
 
-    }
+    // handleImgClick = id => {
+    //     // check if clicked is false
+    //     // if so, set to true
+    //     // add point
+    //     // shuffle cards
+
+    //     // if clicked
+    //     // set top score
+    //     // reset everything
+
+    //     const clickedCharacter = this.state.characters.filter(characters => characters.id === id);
+    //         if(clickedCharacter[0].clicked) {
+
+    //         }
+    // }
+
+
 
     // handle click
     // handle card shuffle
@@ -35,19 +67,19 @@ class App extends React.Component {
             <Wrapper>
                 <Header score={this.state.correctGuesses} topScore={this.state.topScore}/>
                 <div className="row">
-                    {this.state.characters.map(character => (
-                        <CharacterCard
+                    {this.state.characters.map((character) => {
+                        return <CharacterCard
                             key={character.id}
+                            id={character.id}
                             image={character.image}
-                            handleCardClick={this.handleCardClick}
+                            handleOnClick={this.handleImgClick}
                         />
-                    ))}
+                    })}
                 </div>
-
             </Wrapper>
-        )
-    }
+        );
+    };
 
-}
+};
 
 export default App;
--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -8,16 +8,17 @@ const styles = {
 
 class CharacterCard extends React.Component {
 
+    handleImgClick = () => {
+        this.props.handleOnClick(this.props.id);
+    };
+
     render() {
         return (
             <div className="col-lg-2">
-                <div className="card" onClick={this.handleCardClick}>
-                    <div className="img-container" id={this.props.id}>
-                        <img src={this.props.image} alt={this.props.name} style={styles.img}/>
-                    </div>
+                <div className="img-container" id={this.props.id}>
+                    <img src={this.props.image} alt={this.props.name} style={styles.img} onClick={this.handleImgClick}/>
                 </div>
             </div>
-
         );
     };
 
