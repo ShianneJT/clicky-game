9182d3ef698af9e483cd143c915363261ebaebe2
Shianne <ShianneJT@gmail.com>
2020-07-15 19:39:23 -0600

    Updates

---
+++ precache-manifest.a4a280a78d9b5ba886c630ed28c9be9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367e24929bf73f8ac3795a55021244dd",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "49bf596f8e0f7342917a",
+    "url": "/clicky-game/static/css/main.4c77c43b.chunk.css"
+  },
+  {
+    "revision": "7a4b2c96263aa5b61d85",
+    "url": "/clicky-game/static/js/2.69c96d4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.69c96d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bf596f8e0f7342917a",
+    "url": "/clicky-game/static/js/main.cc928d2e.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
